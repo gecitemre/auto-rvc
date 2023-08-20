623bad18a8aa19aff900bd9b529b512f30afb5b3
gecitemre <egecit@trustlab.com>
2023-08-20 15:09:27 +0300

    Add config comments

--- app/front-end/src/config.js
+++ app/front-end/src/config.js
@@ -1,4 +1,5 @@
 const config = {
     IP: '10.4.22.75',
-    PORT: '8000',
+    PORT: '8000', // 7865 is the default port, I use 8000 for development
+    // Change it in Retrieval-based-Voice-Conversion-WebUI/config.py
 };
